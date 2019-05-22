9ca4cec8add83f77116c6927dff2f36e04821b80
Syamala Myneni <syamam1009@gmail.com>
2019-05-22 07:15:14 -0500

    Enabled blog

---
+++ precache-manifest.f2546eebeeb32e7c25295ddee5a9af53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ec75887027a910870721fbff27de45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c226a551777d62032039",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c226a551777d62032039",
+    "url": "/static/js/main.0edb480f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
